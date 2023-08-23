2a62548b161e94458a662d1f66cffa0bb60e0483
michael <michael@mac>
2023-08-23 22:36:18 +0200

    cleanup

---
+++ src/main/kotlin/com/net128/cli/jackson1/DataContainer.kt
@@ -0,0 +1,50 @@
+package com.net128.cli.jackson1
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+
+data class DataContainer(var entries: Map<DataKey, Int>) {
+    fun toJson(): String = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(entries)
+    fun fromJson(json: String) : Map<DataKey, Int> = mapper.readValue(json)
+    companion object {
+        private val module = SimpleModule()
+            .addKeySerializer(DataKey::class.java, DataKeySerializer())
+            .addKeyDeserializer(DataKey::class.java, DataKeyDeserializer())
+        val mapper: ObjectMapper = ObjectMapper().registerKotlinModule().registerModule(module)
+    }
+    class DataKeySerializer: JsonSerializer<DataKey>() {
+        override fun serialize(value: DataKey?, gen: JsonGenerator?, serializers: SerializerProvider?) {
+            gen?.let { jGen ->
+                value?.let {jGen.writeFieldName(mapper.writeValueAsString(it))
+                } ?: jGen.writeNull()
+            }
+        }
+    }
+
+    class DataKeyDeserializer: KeyDeserializer() {
+        override fun deserializeKey(key: String?, ctxt: DeserializationContext?): DataKey? {
+            return key?.let { mapper.readValue(key) }
+        }
+    }
+}
+
+data class DataKey (
+    val keyPart1: KeyPart1,
+    val keyPart2: KeyPart2,
+    val keyPart3: KeyPart3
+)
+
+enum class KeyPart1 { High, Low  }
+
+data class KeyPart2 (
+    val id: Int,
+    val name: String
+)
+
+data class KeyPart3 (
+        val value: Int
+)
+
--- src/main/kotlin/com/net128/cli/jackson1/Jackson1Application.kt
+++ src/main/kotlin/com/net128/cli/jackson1/Jackson1Application.kt
@@ -3,21 +3,20 @@ package com.net128.cli.jackson1
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import com.fasterxml.jackson.module.kotlin.readValue
 
 @SpringBootApplication
 class Jackson1Application : CommandLineRunner {
     override fun run(vararg args: String) {
-        val map = mapOf(
-                DataKey(KeyPart1.high, KeyPart2(1, "First"), KeyPart3(1234)) to 100,
-                DataKey(KeyPart1.low, KeyPart2(2, "Second"), KeyPart3(4567)) to 200
-        )
+        val dataContainer = DataContainer(mapOf(
+            DataKey(KeyPart1.High, KeyPart2(1, "First"), KeyPart3(1234)) to 100,
+            DataKey(KeyPart1.Low, KeyPart2(2, "Second"), KeyPart3(4567)) to 200
+        ))
 
-        val jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map)
+        val jsonString = dataContainer.toJson()
         println(jsonString)
 
-        val map2: Map<DataKey, Int> = mapper.readValue(jsonString/*, mapType*/)
-        if(map != map2) println("read value mismatch")
+        val conainer2 = DataContainer(dataContainer.fromJson(jsonString))
+        if(dataContainer != conainer2) println("read value mismatch")
     }
 }
 
